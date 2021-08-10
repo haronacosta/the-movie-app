1b1771637e243afe75509bc36b93d110ef138d5e
Haron Acosta <haron.acosta14@gmail.com>
2021-08-09 20:10:24 -0400

    Github pages script and images size

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const apiKey = '3b5c31ed638d62a93f1bab6729057395';
 
-  const imagesUrl = 'https://image.tmdb.org/t/p/original/';
+  const imagesUrl = 'https://image.tmdb.org/t/p/w500/';
 
   const [movies, setMovies] = useState([]);
 
@@ -18,7 +18,7 @@ function App() {
       .then((resp) => resp.json())
       .then(({ results }) => {
         results.forEach((movie) => {
-          fetch(`${apiUrl}/movie/${movie.id}?api_key=${apiKey}&language=en-US`)
+          fetch(`${apiUrl}movie/${movie.id}?api_key=${apiKey}&language=en-US`)
             .then((resp) => resp.json())
             .then((details) => {
               setMovies((movs) => [
