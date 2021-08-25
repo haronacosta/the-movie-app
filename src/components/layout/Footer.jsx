ada694829b512ed966f2e0b71e230cc1d0491942
Haron Acosta <haron.acosta14@gmail.com>
2021-08-25 12:06:23 -0400

    The Movie DB logo in footer fixed

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="Footer">
-    Code by <a href="https://github.com">Haron Acosta</a>. Movies content from
+    Code by <a href="https://github.com">Haron Acosta</a>.{' '}
+    <span> Movies content from</span>
     <a href="https://www.themoviedb.org/">
       <img
         src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
